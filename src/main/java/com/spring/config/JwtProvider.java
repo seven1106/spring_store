4b3140ed786a63a03116f6adc77151ab68d6f448
seven1106 <that9011@gmail.com>
2024-06-30 23:27:28 +0700

    token provider

---
+++ src/main/java/com/spring/config/JwtProvider.java
@@ -0,0 +1,38 @@
+package com.spring.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class JwtProvider {
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+    public String generateToken(Authentication auth ) {
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles = populateAuthorities(authorities);
+        String jwt = Jwts.builder()
+                .setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 864000000))// 10 days in milliseconds
+                .claim("email", auth.getName())
+                .claim("authorities", roles)
+                .signWith(secretKey)
+                .compact();
+        return jwt;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auths = new HashSet<>();
+
+        for (GrantedAuthority authority : authorities) {
+            auths.add(authority.getAuthority());
+        }
+        return String.join(",", auths);
+    }
+}
